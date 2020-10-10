4ce52e313d23e7109e9b0fcae68278f8e35ad14f
Una Thompson <una@unascribed.com>
2020-10-10 15:58:26 -0700

    Bump version, allow using on 1.16.1/2

--- forge-1.16/src/main/java/com/unascribed/ears/EarsMod.java
+++ forge-1.16/src/main/java/com/unascribed/ears/EarsMod.java
@@ -1,7 +1,17 @@
 package com.unascribed.ears;
 
+import net.minecraftforge.fml.common.LoaderException;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("ears")
 public class EarsMod {
+	
+	public EarsMod() {
+		try {
+			Class.forName("org.spongepowered.asm.mixin.Mixins");
+		} catch (Throwable t) {
+			throw new LoaderException("Ears requires MixinBootstrap on versions earlier than Forge 32.0.72");
+		}
+	}
+	
 }
