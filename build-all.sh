c2c5f3686648dc2d9d986cf6f33aab2975ea5b60
Una Thompson <una@unascribed.com>
2020-11-10 03:56:05 -0800

    Shade common, misc

--- build-all.sh
+++ build-all.sh
@@ -3,11 +3,12 @@
 rm artifacts/*
 for proj in fabric-1.14 fabric-1.16 forge-1.8 forge-1.12 forge-1.14 forge-1.15 forge-1.16; do
 	(
+		echo $proj...
 		cd $proj
 		TERM=dumb chronic ./gradlew clean build
-	) &
+	)
 done
 wait
 mkdir -p artifacts
 cp */build/libs/* artifacts
-rm artifacts/*-sources{,-dev}.jar
+rm artifacts/*-sources{,-dev}.jar artifacts/common.jar
