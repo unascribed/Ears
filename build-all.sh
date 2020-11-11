661790c8c8020daeef4e293c6da5fbf00821b9fc
Una Thompson <una@unascribed.com>
2020-11-10 18:18:27 -0800

    Oh yeah

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm artifacts/*
-for proj in fabric-1.14 fabric-1.16 fabric-1.17 forge-1.7 forge-1.8 forge-1.12 forge-1.14 forge-1.15 forge-1.16; do
+for proj in fabric-1.14 fabric-1.16 fabric-1.17 forge-1.7 forge-1.8 forge-1.12 forge-1.14 forge-1.15 forge-1.16 rift-1.13; do
 	(
 		echo $proj...
 		cd $proj
