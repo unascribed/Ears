cdbde33ff54af52bd29ce7236d97de4e8d0ca0c9
Una Thompson <una@unascribed.com>
2021-08-27 02:44:25 -0700

    Add a script for copying the mods to my MultiMC for testing

---
+++ install-test.fish
@@ -0,0 +1,19 @@
+#!/usr/bin/fish
+set coremod ears-forge-1.4 ears-forge-1.5
+set agent ears-nfc
+for t in (ls -1 artifacts/ |rev |cut -d- -f 2- |rev)
+	if contains $t $agent
+		rm -f ~/MultiMC/instances/$t/.minecraft/ears.jar
+		cp artifacts/$t*.jar ~/MultiMC/instances/$t/.minecraft/ears.jar
+	else 
+		set dir mods
+		if contains $t $coremod
+			set dir coremods
+		end
+		mkdir -p ~/MultiMC/instances/$t/.minecraft/$dir/
+		rm -f ~/MultiMC/instances/$t/.minecraft/$dir/ears-*.jar
+		cp artifacts/$t*.jar ~/MultiMC/instances/$t/.minecraft/$dir
+	end
+end
+rm -f ~/MultiMC/instances/ears-fabric-1.15/.minecraft/mods/ears-*.jar
+cp artifacts/ears-fabric-1.16*.jar ~/MultiMC/instances/ears-fabric-1.15/.minecraft/mods
