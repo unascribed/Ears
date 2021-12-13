078d4bcf307d9612dde9c40bb846026473c83f6c
Una Thompson <una@unascribed.com>
2021-12-13 13:32:20 -0800

    build.sh robustness

--- build.sh
+++ build.sh
@@ -1,10 +1,53 @@
 #!/bin/bash -e
 
+if [[ "$(uname -s)" =~ ^CYGWIN || "$(uname -s)" =~ ^MINGW || "$(uname -s)" =~ ^MSYS ]]; then
+	echo "WARNING: Building Ears on Windows, even under a Linux-like environment, is not supported. Continue at your own peril."
+	sleep 3
+fi
+
+if [[ -z "$JAVA8_HOME" || -z "$JAVA11_HOME" || -z "$JAVA17_HOME" ]]; then
+	echo "Building Ears requires Java 8, Java 11, and Java 17." 1>&2
+	echo "Please install them and set the JAVA8_HOME, JAVA11_HOME, and JAVA17_HOME env vars." 1>&2
+	echo "You can get all three of these from https://adoptium.net/" 1>&2
+	exit 1
+fi
+
+if [ -n "$JAVA11_HOME" ]; then
+	export JAVA_HOME=$JAVA11_HOME
+fi
+if [ -z "$JAVA16_HOME" ]; then
+	export JAVA16_HOME=$JAVA17_HOME
+fi
+
+check_java() {
+	v=$(env -u_JAVA_OPTIONS $JAVA_HOME/bin/java -Xint -version 2>&1 | head -n1 |cut -d\" -f2)
+	query=$1
+	regex=$2
+	if [ -n "$3" ]; then
+		query="$2 or $3"
+		regex="$2|$3"
+	fi
+	highlighted=$(echo $v |grep -E --color=always "^($regex)" || (echo "Expected JAVA$1_HOME to point to Java $query, but instead it points at Java $v. Stop" 1>&2 && exit 2))
+	color=32
+	if [ -n "$3" ]; then
+		echo $v |grep -q "^$3" >/dev/null && color=33
+	fi
+	highlighted=$(echo $highlighted |sed "s/31/$color/")
+	echo "Java $1: $highlighted"
+}
+
+JAVA_HOME=$JAVA8_HOME check_java 8 1.8
+check_java 11 11
+JAVA_HOME=$JAVA16_HOME check_java 16 16 17
+JAVA_HOME=$JAVA17_HOME check_java 17 17
+echo "Looks good."
+echo
+
 normal="fabric-1.8 fabric-1.14 fabric-1.16 forge-1.12 forge-1.14 forge-1.15 forge-1.16 fabric-b1.7.3 rift-1.13"
 needsJ8="forge-1.6 forge-1.7 forge-1.8 forge-1.9"
 needsJ16="fabric-1.17 forge-1.17"
 needsJ17="forge-1.18"
-# these ones can't be built in parallel
+# these ones can't be built in parallel (or build so quickly that we shouldn't bother)
 special="nfc nsss forge-1.2 forge-1.4 forge-1.5"
 
 toBuild=" $normal $needsJ8 $needsJ16 $needsJ17 $special "
@@ -44,12 +87,8 @@ echo "Building $count platform$s..."
 build $normal
 build $nobodyCares
 JAVA_HOME=$JAVA8_HOME build $needsJ8
-if [ -n "$JAVA16_HOME" ]; then
-	JAVA_HOME=$JAVA16_HOME build $needsJ16
-else
-	JAVA_HOME=$JAVA17_HOME build $needsJ16
-fi
-JAVA_HOME=$JAVA17_HOME build $needsJ16
+JAVA_HOME=$JAVA16_HOME build $needsJ16
+JAVA_HOME=$JAVA17_HOME build $needsJ17
 wait
 for proj in $special; do
 	(
