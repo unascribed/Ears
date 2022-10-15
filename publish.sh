ff4fa70ff6a59ee2860220151f085f1cc5a09acc
Una Thompson <me@unascribed.com>
2022-10-15 02:05:41 -0700

    Mark as client-only

--- publish.sh
+++ publish.sh
@@ -35,24 +35,24 @@ if [ "$3" != "-" ]; then
 	quilt=11
 	rift=3
 	other=10
-	./mcmodcn-upload.sh $classID "Beta 1.7.3" $fabric '' artifacts/ears-fabric-b1.7.3-*.jar
-	./mcmodcn-upload.sh $classID "1.2.5" $other '' artifacts/ears-forge-1.2-*.jar
-	./mcmodcn-upload.sh $classID "1.4.7" $forge '' artifacts/ears-forge-1.4-*.jar
-	./mcmodcn-upload.sh $classID "1.5.2" $forge '' artifacts/ears-forge-1.5-*.jar
-	./mcmodcn-upload.sh $classID "1.6.4" $forge '' artifacts/ears-forge-1.6-*.jar
-	./mcmodcn-upload.sh $classID "1.7.10" $forge '' artifacts/ears-forge-1.7-*.jar
-	./mcmodcn-upload.sh $classID "1.8" $forge '' artifacts/ears-forge-1.8-*.jar
-	./mcmodcn-upload.sh $classID "1.9/10/11" $forge '' artifacts/ears-forge-1.9-*.jar
-	./mcmodcn-upload.sh $classID "1.12" $forge '' artifacts/ears-forge-1.12-*.jar
-	./mcmodcn-upload.sh $classID "1.13.2" $rift '' artifacts/ears-rift-1.13-*.jar
-	./mcmodcn-upload.sh $classID "1.14" $fabric '' artifacts/ears-fabric-1.14-*.jar
-	./mcmodcn-upload.sh $classID "1.14" $forge '' artifacts/ears-forge-1.14-*.jar
-	./mcmodcn-upload.sh $classID "1.15" $forge '' artifacts/ears-forge-1.15-*.jar
-	./mcmodcn-upload.sh $classID "1.15/16" $fabric '' artifacts/ears-fabric-1.16-*.jar
-	./mcmodcn-upload.sh $classID "1.16" $forge '' artifacts/ears-forge-1.16-*.jar
-	./mcmodcn-upload.sh $classID "1.17/18" $fabric,$quilt '' artifacts/ears-fabric-1.17-*.jar
-	./mcmodcn-upload.sh $classID "1.17" $forge '' artifacts/ears-forge-1.17-*.jar
-	./mcmodcn-upload.sh $classID "1.18" $forge '' artifacts/ears-forge-1.18-*.jar
-	./mcmodcn-upload.sh $classID "1.19" $fabric,$quilt '' artifacts/ears-fabric-1.19-*.jar
-	./mcmodcn-upload.sh $classID "1.19" $forge '' artifacts/ears-forge-1.19-*.jar
+	./mcmodcn-upload.sh $classID "Beta 1.7.3" $fabric 'client' artifacts/ears-fabric-b1.7.3-*.jar
+	./mcmodcn-upload.sh $classID "1.2.5" $other 'client' artifacts/ears-forge-1.2-*.jar
+	./mcmodcn-upload.sh $classID "1.4.7" $forge 'client' artifacts/ears-forge-1.4-*.jar
+	./mcmodcn-upload.sh $classID "1.5.2" $forge 'client' artifacts/ears-forge-1.5-*.jar
+	./mcmodcn-upload.sh $classID "1.6.4" $forge 'client' artifacts/ears-forge-1.6-*.jar
+	./mcmodcn-upload.sh $classID "1.7.10" $forge 'client' artifacts/ears-forge-1.7-*.jar
+	./mcmodcn-upload.sh $classID "1.8" $forge 'client' artifacts/ears-forge-1.8-*.jar
+	./mcmodcn-upload.sh $classID "1.9/10/11" $forge 'client' artifacts/ears-forge-1.9-*.jar
+	./mcmodcn-upload.sh $classID "1.12" $forge 'client' artifacts/ears-forge-1.12-*.jar
+	./mcmodcn-upload.sh $classID "1.13.2" $rift 'client' artifacts/ears-rift-1.13-*.jar
+	./mcmodcn-upload.sh $classID "1.14" $fabric 'client' artifacts/ears-fabric-1.14-*.jar
+	./mcmodcn-upload.sh $classID "1.14" $forge 'client' artifacts/ears-forge-1.14-*.jar
+	./mcmodcn-upload.sh $classID "1.15" $forge 'client' artifacts/ears-forge-1.15-*.jar
+	./mcmodcn-upload.sh $classID "1.15/16" $fabric 'client' artifacts/ears-fabric-1.16-*.jar
+	./mcmodcn-upload.sh $classID "1.16" $forge 'client' artifacts/ears-forge-1.16-*.jar
+	./mcmodcn-upload.sh $classID "1.17/18" $fabric,$quilt 'client' artifacts/ears-fabric-1.17-*.jar
+	./mcmodcn-upload.sh $classID "1.17" $forge 'client' artifacts/ears-forge-1.17-*.jar
+	./mcmodcn-upload.sh $classID "1.18" $forge 'client' artifacts/ears-forge-1.18-*.jar
+	./mcmodcn-upload.sh $classID "1.19" $fabric,$quilt 'client' artifacts/ears-fabric-1.19-*.jar
+	./mcmodcn-upload.sh $classID "1.19" $forge 'client' artifacts/ears-forge-1.19-*.jar
 fi
