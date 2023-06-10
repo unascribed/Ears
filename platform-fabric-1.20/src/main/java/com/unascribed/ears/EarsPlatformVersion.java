1734871407c94091af96a27d51622f099a5314ab
Una Thompson <me@unascribed.com>
2023-06-10 02:14:22 -0700

    Integrate 1.20 port with build mess, changelog

--- platform-fabric-1.20/src/main/java/com/unascribed/ears/EarsPlatformVersion.java
+++ platform-fabric-1.20/src/main/java/com/unascribed/ears/EarsPlatformVersion.java
@@ -3,6 +3,6 @@ package com.unascribed.ears;
 public class EarsPlatformVersion {
 
 	public static final String VERSION = /*VERSION*/"1.4.6"/*/VERSION*/;
-	public static final String KIND = "Fabric 1.19.4";
+	public static final String KIND = "Fabric 1.20";
 	
 }
