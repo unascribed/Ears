08e0936795c102d5016c330813f7cf34c681b879
Una Kearney <me@unascribed.com>
2025-02-05 23:58:46 -0500

    And they don't stop coming

--- common/src/agent/java/com/unascribed/ears/common/agent/mini/exception/PointerNotSetException.java
+++ common/src/agent/java/com/unascribed/ears/common/agent/mini/exception/PointerNotSetException.java
@@ -3,7 +3,7 @@
  * 
  * The MIT License
  *
- * Copyright (c) 2017-2021 Una Thompson (unascribed) and contributors
+ * Copyright (c) 2017-2021 Una Kearney (unascribed) and contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
