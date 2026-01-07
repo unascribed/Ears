9d8fce7ff5c6354b3a0a5d4d0ff74d5e5cfc2ed2
Una Kearney <me@unascribed.com>
2026-01-06 19:10:19 -0500

    Blah

--- platform-fabric-1.21.10/src/main/java/com/unascribed/ears/EarsPlatformVersion.java
+++ platform-fabric-1.21.10/src/main/java/com/unascribed/ears/EarsPlatformVersion.java
@@ -2,7 +2,7 @@ package com.unascribed.ears;
 
 public class EarsPlatformVersion {
 
-	public static final String VERSION = /*VERSION*/"1.4.7"/*/VERSION*/;
+	public static final String VERSION = /*VERSION*/"1.4.7_01"/*/VERSION*/;
 	public static final String KIND = "Fabric 1.21.10";
 	
 }
--- publish.sh
+++ publish.sh
@@ -29,12 +29,12 @@ else
 		rift-1.13
 		stapi-b1.7.3
 		fabric-1.14 fabric-1.16 fabric-1.17 fabric-1.19 fabric-1.19.3 fabric-1.19.4 fabric-1.20
-		fabric-1.20.2 fabric-1.20.6 fabric-1.21 fabric-1.21.4 fabric-1.21.5 fabric-1.21.10
+		fabric-1.20.2 fabric-1.20.6 fabric-1.21 fabric-1.21.4 fabric-1.21.5 fabric-1.21.10 fabric-1.21.11
 		
 		forge-1.2 forge-1.4 forge-1.5 forge-1.6 forge-1.7 forge-1.8 forge-1.9 forge-1.12 forge-1.14
 		forge-1.15 forge-1.16 forge-1.17 forge-1.18 forge-1.19 forge-1.19.3 forge-1.19.4
 		
-		neoforge-1.20.2 neoforge-1.21 neoforge-1.21.4 neoforge-1.21.5 neoforge-1.21.10"
+		neoforge-1.20.2 neoforge-1.21 neoforge-1.21.4 neoforge-1.21.5 neoforge-1.21.10 neoforge-1.21.11"
 	curse="$common"
 	modrinth="$common"
 	mcmodcn="$common"
@@ -192,6 +192,10 @@ if [ "$MCMODCN_COOKIE" != "-" ]; then
 				title="1.21.10"
 				loaders="$fabric,$quilt"
 			;;
+			fabric-1.21.11)
+				title="1.21.11"
+				loaders="$fabric,$quilt"
+			;;
 			neoforge-1.20.2)
 				title="1.20.2"
 				loaders="$neoforge"
@@ -210,6 +214,10 @@ if [ "$MCMODCN_COOKIE" != "-" ]; then
 			;;
 			neoforge-1.21.10)
 				title="1.21.10"
+				loaders="$neoforge"
+			;;
+			neoforge-1.21.11)
+				title="1.21.11"
 				loaders="$neoforge"
 			;;
 			*)
